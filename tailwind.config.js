e808aeab38c890d3eaf9efe6020f5885bb63893f
sandoramix <sandoramixer61@gmail.com>
2022-11-26 17:05:33 +0100

    basic tailwindcss config with postcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: `'Helvetica Neue', Arial, Helvetica, sans-serif`
+      }
+    },
+  },
+  plugins: [],
+};
