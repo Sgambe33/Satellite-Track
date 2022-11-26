5fbfb48a7893d715afccc6edc945abd896c5c8ce
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2022-11-26 21:56:46 +0100

    Tried to use DB func

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require(`express`);
 const http = require(`http`);
 const path = require("path");
-const { getLatLngObj } = require("tle.js");
+const {db, rigaspecifica} = require("./src/js/db_fun")
+
 
 const SETTINGS = {
 	port: 80,
@@ -20,20 +21,35 @@ app.use('/tle', express.static(`./node_modules/tle.js/dist`));
 
 app.use(`/get-tle`, (req, res) => {
 	try {
-		const { tle, timestamp } = req.body;
-		return res.status(200).json(getLatLngObj(tle, timestamp));
+		//const { tle, timestamp } = req.body;
+		return res.status(200);
 
 	} catch (error) {
+		console.log(error)
 		return res.status(400).json({
 			message: `Invalid TLE`
 		});
 	}
 });
 
-
-
 const http_server = http.createServer(app);
 
 http_server.listen(SETTINGS.port, SETTINGS.host, () => {
 	console.log(`Server on at http://localhost:${SETTINGS.port}`);
+});
+
+let sql = `SELECT * FROM satcat WHERE OBJECT_NAME  = ?`;
+let nomeSatellite = 'COSMOS 1387';
+
+app.use(`/primariga`, (req, res) => {
+	try {
+		let resp = rigaspecifica(sql, nomeSatellite)
+		return res.status(200).json(resp);
+
+	} catch (error) {
+		console.log(error)
+		return res.status(400).json({
+			message: `Errore`
+		});
+	}
 });
